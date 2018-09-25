6f19ae54f00364c3001fa94fc2195ce3883aebe6
Bláha Marek <blaha.marcus@gmail.com>
2018-09-25 20:02:28 +0200

    Pridanie stranky

---
+++ TP propagacna stranka/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $("#toProject").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#project").offset().top
+        }, 800);
+    });
+    $("#toMembers").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#members").offset().top
+        }, 800);
+    });
+    $("#toDocs").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#docs").offset().top
+        }, 800);
+    });
+    $("#up").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#intro").offset().top
+        }, 800);
+    });
+
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        if(scroll > 400) {
+            $("#up").fadeIn();
+        }
+        else{
+            $("#up").fadeOut();
+        }
+        
+    });
+
+});
