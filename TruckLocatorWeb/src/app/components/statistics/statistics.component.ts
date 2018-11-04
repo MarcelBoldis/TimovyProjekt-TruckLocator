94d14f04a7410df7c921cf268618f144a7d873e1
Blahos <blaha.marcus@gmail.com>
2018-11-04 22:23:25 +0100

    drobne opravy

--- TruckLocatorWeb/src/app/components/statistics/statistics.component.ts
+++ TruckLocatorWeb/src/app/components/statistics/statistics.component.ts
@@ -8,52 +8,61 @@ import { Chart } from 'chart.js';
 })
 
 export class StatisticsComponent implements OnInit {
+
+  charts = [true, false];
+
   LineChart = [];
   DoughnutChart = [];
   constructor() { }
 
   ngOnInit() {
-    this.LineChart = new Chart('lineChart', {
-      type: 'line',
-      data: {
-        labels: ['Jan', 'Feb', 'March', 'April', 'May'],
-        datasets: [
-          {
-            data: [9, 7, 5, 10, 6], // your data array
-            borderColor: '#00AEFF',
-            fill: false
+    this.showChart();
+  }
+  showChart() {
+    if (this.charts[0]) {
+      this.LineChart = new Chart('myChart', {
+        type: 'line',
+        data: {
+          labels: ['Jan', 'Feb', 'March', 'April', 'May'],
+          datasets: [
+            {
+              data: [9, 7, 5, 10, 6], // your data array
+              borderColor: '#00AEFF',
+              fill: false
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          title: {
+            text: 'Chart',
+            display: true
+          },
+          animation: {
+            duration: 1000
           }
-        ]
-      },
-      options: {
-        legend: {
-          display: false
-        },
-        title: {
-          text: 'Chart',
-          display: true
-        },
-        animation: {
-          duration: 3000
         }
-      }
-    });
+      });
+    } else {
 
-    this.DoughnutChart = new Chart('doughnutChart', {
-      type: 'doughnut',
-      data: {
-        datasets: [{
+
+      this.DoughnutChart = new Chart('myChart', {
+        type: 'doughnut',
+        data: {
+          datasets: [{
             data: [10, 20, 30]
-        }],
-        labels: ['Red', 'Yellow', 'Blue']
-      },
-    });
+          }],
+          labels: ['Red', 'Yellow', 'Blue']
+        },
+      });
+    }
   }
 
   showGraph(index: number, para: string) {
-    // const destroy: HTMLElement = document.getElementById('chartDiv');
-    // destroy.innerHTML = '';
-    const element: HTMLElement = document.getElementById(para);
-    element.setAttribute('style', 'display:block;');
+    this.charts.fill(false);
+    this.charts[index] = true;
+    this.showChart();
   }
 }
